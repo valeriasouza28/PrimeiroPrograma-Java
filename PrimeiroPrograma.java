bc42b1eddfa03266e7b638fe8d972817d2b6e1bb
valeriasouza28 <82419677+valeriasouza28@users.noreply.github.com>
2021-06-12 21:55:14 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package primeiroprograma;
+import java.lang.System;
+
+public class PrimeiroPrograma
+{
+    public static void main(String[] args)
+	{
+{
+		System.out.println("Ola, Mundo!") ;
+		
+	}
+	
+	}
+	
+} 
+
